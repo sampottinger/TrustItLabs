472b91cc61b0087017b82c09947f59560a167fcf
A. Samuel Pottinger <Samnsparky@gmail.com>
2012-03-31 18:07:34 -0600

    Immediate operation test suite controllers
    All of the suite controllers that only operate on suites. This does not
    include the TestSuiteSampleController

---
+++ app/landing/controllers/suite.py
@@ -0,0 +1,94 @@
+"""
+Application logic for controlling collections of tests as test suites
+
+@auth: Sam Pottinger
+"""
+
+from ..util import singleton
+
+@singleton
+class TestSuiteRenderContoller:
+    """ Logic for generating the AJAX capable test suite pages """
+
+    def __init__(self):
+        pass
+
+    def render_suite_views(self, session):
+        """
+        Renders the test suite view and all of its subordinates
+        
+        @param session: Session key value pairs
+        @type session: Dict
+        @return: The rendered test suite views
+        @rtype: String
+        """
+        raise NotImplementedError("Not yet implemented")
+
+@singleton
+class TestSuiteConfigurationController:
+    """ CRUD controller for test suites """
+
+    def __init__(self):
+        pass
+
+    def create_suite(self, suite_info, session):
+        """
+        Exposed logic for creating a test suite
+
+        @param suite_info: Structured information regarding a suite. Should be parsed
+                           YAML or equivalent from client.
+        @type suite_info: Dict
+        @param session: key value pairs with all saved session values
+        @type session: dict
+        @note: Looking for name and sample id. Data not expected to be cleaned
+               beforehand.
+        @return: Serialized version of the newly created suite
+        @rtype: Dict
+        """
+        raise NotImplementedError("Not yet implemented")
+
+    def read_suite(self, suite_id, session):
+        """
+        Exposed logic for reading a suite from the database
+
+        @param suite_id: The unique numerical id of the suite whose information
+                         is being requested
+        @type suite_id: Integer
+        @param session: key value pairs with all saved session values
+        @type session: dict
+        @return: Serialized version of the requested suite if access allowed
+                 and None otherwise
+        @rtype: Dict
+        @note: User credentials will be checked by controller
+        """
+        raise NotImplementedError("Not yet implemented")
+
+    def update_suite(self, suite_info, session):
+        """
+        Exposed logic for updating an existing suite in the database
+
+        @param suite_info: Structured information that should replace current suite
+                           information. Should be parsed YAML or equivalent from
+                           client.
+        @type suite_info: Dict
+        @param session: key value pairs with all saved session values
+        @type session: dict
+        @return: True if successful or error message otherwise 
+        @rtype: Boolean or String
+        @note: This controller will check user access control
+        """
+        raise NotImplementedError("Not yet implemented")
+
+    def delete_suite(self, suite_id, session):
+        """
+        Exposed logic for deleting an existing suite in the database
+
+        @param suite_id: The unique integer id of the suite to delete
+        @type suite_id: Integer
+        @param session: key value pairs with all saved session values
+        @type session: dict
+        @return: True if successful or error message otherwise 
+        @rtype: Boolean or String
+        @note: This controller will check user access control
+        """
+        raise NotImplementedError("Not yet implemented")
